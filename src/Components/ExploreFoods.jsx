42bd68bcea667a04a3fc5a7bba1c41044471d6fb
HGhulamHamza <khawajahamza566@gmail.com>
2024-08-04 10:36:56 +0500

    Import Images

--- src/Components/ExploreFoods.jsx
+++ src/Components/ExploreFoods.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import './ExploreFoods.css'; 
-
+import Pizza from '../assets/pizza.jpg';
+import Burger from '../assets/burger.jpg';
+import Momos from '../assets/momos.jpg';
 const ExploreFoods = () => {
   return (
     <div className="explore-foods">
       <h2>Explore Foods</h2>
       <div className="food-cards">
         <div className="food-card">
-          <img src="src/assets/pizza.jpg" alt="Pizza" className="food-image" />
+          <img src={Pizza} alt="Pizza" className="food-image" />
           <p className="food-name">Pizza</p>
         </div>
         <div className="food-card">
-          <img src="src/assets/burger.jpg" alt="Burger" className="food-image" />
+          <img src={Burger} alt="Burger" className="food-image" />
           <p className="food-name">Burger</p>
         </div>
         <div className="food-card">
-          <img src="src/assets/momos.jpg" alt="Momo" className="food-image" />
+          <img src={Momos} alt="Momo" className="food-image" />
           <p className="food-name">Momo</p>
         </div>
       </div>
